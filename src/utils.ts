867cb5a244198eb2ba5aef2f39e7d889094995b4
Matheus Lustosa <lustosa.matheus@gmail.com>
2025-05-11 00:46:03 +0200

    Update TS definitions

--- src/utils.ts
+++ src/utils.ts
@@ -16,10 +16,58 @@ export function curry<A, B, R>(fn: (a: A, b: B) => R) {
   };
 }
 
+export function compose<TArgs extends any[], R1, R2, R3, R4, R5>(
+  f5: (a: R4) => R5,
+  f4: (a: R3) => R4,
+  f3: (a: R2) => R3,
+  f2: (a: R1) => R2,
+  f1: (...args: TArgs) => R1,
+): (...args: TArgs) => R5;
+export function compose<TArgs extends any[], R1, R2, R3, R4>(
+  f4: (a: R3) => R4,
+  f3: (a: R2) => R3,
+  f2: (a: R1) => R2,
+  f1: (...args: TArgs) => R1,
+): (...args: TArgs) => R4;
+export function compose<TArgs extends any[], R1, R2, R3>(
+  f3: (a: R2) => R3,
+  f2: (a: R1) => R2,
+  f1: (...args: TArgs) => R1,
+): (...args: TArgs) => R3;
+export function compose<TArgs extends any[], R1, R2>(
+  f2: (a: R1) => R2,
+  f1: (...args: TArgs) => R1,
+): (...args: TArgs) => R2;
+export function compose<TArgs extends any[], R1>(
+  f1: (...args: TArgs) => R1,
+): (...args: TArgs) => R1;
 export function compose<T>(...fns: Array<(arg: T) => T>) {
   return (x: T) => fns.reduceRight((acc, fn) => fn(acc), x);
 }
 
+export function pipe<TArgs extends any[], R1, R2, R3, R4, R5>(
+  f1: (...args: TArgs) => R1,
+  f2: (a: R1) => R2,
+  f3: (a: R2) => R3,
+  f4: (a: R3) => R4,
+  f5: (a: R4) => R5,
+): (...args: TArgs) => R5;
+export function pipe<TArgs extends any[], R1, R2, R3, R4>(
+  f1: (...args: TArgs) => R1,
+  f2: (a: R1) => R2,
+  f3: (a: R2) => R3,
+  f4: (a: R3) => R4,
+): (...args: TArgs) => R4;
+export function pipe<TArgs extends any[], R1, R2, R3>(
+  f1: (...args: TArgs) => R1,
+  f2: (a: R1) => R2,
+  f3: (a: R2) => R3,
+): (...args: TArgs) => R3;
+export function pipe<TArgs extends any[], R1, R2>(
+  f1: (...args: TArgs) => R1,
+  f2: (a: R1) => R2,
+): (...args: TArgs) => R2;
+export function pipe<TArgs extends any[], R1>(f1: (...args: TArgs) => R1): (...args: TArgs) => R1;
 export function pipe<T>(...fns: Array<(arg: T) => T>) {
   return (x: T) => fns.reduce((acc, fn) => fn(acc), x);
 }
