5512e0ec398e912f95295c2a9facbcf43f4e3ab4
Matheus Lustosa <lustosa.matheus@gmail.com>
2025-11-18 12:08:42 +0100

    Update types

--- src/utils.ts
+++ src/utils.ts
@@ -16,6 +16,23 @@ export function curry<A, B, R>(fn: (a: A, b: B) => R) {
   };
 }
 
+export function compose<TArgs extends any[], R1, R2, R3, R4, R5, R6, R7>(
+  f7: (a: R6) => R7,
+  f6: (a: R5) => R6,
+  f5: (a: R4) => R5,
+  f4: (a: R3) => R4,
+  f3: (a: R2) => R3,
+  f2: (a: R1) => R2,
+  f1: (...args: TArgs) => R1,
+): (...args: TArgs) => R7;
+export function compose<TArgs extends any[], R1, R2, R3, R4, R5, R6>(
+  f6: (a: R5) => R6,
+  f5: (a: R4) => R5,
+  f4: (a: R3) => R4,
+  f3: (a: R2) => R3,
+  f2: (a: R1) => R2,
+  f1: (...args: TArgs) => R1,
+): (...args: TArgs) => R6;
 export function compose<TArgs extends any[], R1, R2, R3, R4, R5>(
   f5: (a: R4) => R5,
   f4: (a: R3) => R4,
@@ -45,6 +62,23 @@ export function compose<T>(...fns: Array<(arg: T) => T>) {
   return (x: T) => fns.reduceRight((acc, fn) => fn(acc), x);
 }
 
+export function pipe<TArgs extends any[], R1, R2, R3, R4, R5, R6, R7>(
+  f1: (...args: TArgs) => R1,
+  f2: (a: R1) => R2,
+  f3: (a: R2) => R3,
+  f4: (a: R3) => R4,
+  f5: (a: R4) => R5,
+  f6: (a: R5) => R6,
+  f7: (a: R6) => R7,
+): (...args: TArgs) => R7;
+export function pipe<TArgs extends any[], R1, R2, R3, R4, R5, R6>(
+  f1: (...args: TArgs) => R1,
+  f2: (a: R1) => R2,
+  f3: (a: R2) => R3,
+  f4: (a: R3) => R4,
+  f5: (a: R4) => R5,
+  f6: (a: R5) => R6,
+): (...args: TArgs) => R6;
 export function pipe<TArgs extends any[], R1, R2, R3, R4, R5>(
   f1: (...args: TArgs) => R1,
   f2: (a: R1) => R2,
