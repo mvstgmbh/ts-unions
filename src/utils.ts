f05a396e76acb67f3465b2a6b65db956ac837448
Matheus Lustosa <lustosa.matheus@gmail.com>
2025-11-28 15:42:21 +0100

    Expand type definitions

--- src/utils.ts
+++ src/utils.ts
@@ -16,6 +16,39 @@ export function curry<A, B, R>(fn: (a: A, b: B) => R) {
   };
 }
 
+export function compose<TArgs extends any[], R1, R2, R3, R4, R5, R6, R7, R8, R9, R10>(
+  f10: (a: R9) => R10,
+  f9: (a: R8) => R9,
+  f8: (a: R7) => R8,
+  f7: (a: R6) => R7,
+  f6: (a: R5) => R6,
+  f5: (a: R4) => R5,
+  f4: (a: R3) => R4,
+  f3: (a: R2) => R3,
+  f2: (a: R1) => R2,
+  f1: (...args: TArgs) => R1,
+): (...args: TArgs) => R10;
+export function compose<TArgs extends any[], R1, R2, R3, R4, R5, R6, R7, R8, R9>(
+  f9: (a: R8) => R9,
+  f8: (a: R7) => R8,
+  f7: (a: R6) => R7,
+  f6: (a: R5) => R6,
+  f5: (a: R4) => R5,
+  f4: (a: R3) => R4,
+  f3: (a: R2) => R3,
+  f2: (a: R1) => R2,
+  f1: (...args: TArgs) => R1,
+): (...args: TArgs) => R9;
+export function compose<TArgs extends any[], R1, R2, R3, R4, R5, R6, R7, R8>(
+  f8: (a: R7) => R8,
+  f7: (a: R6) => R7,
+  f6: (a: R5) => R6,
+  f5: (a: R4) => R5,
+  f4: (a: R3) => R4,
+  f3: (a: R2) => R3,
+  f2: (a: R1) => R2,
+  f1: (...args: TArgs) => R1,
+): (...args: TArgs) => R8;
 export function compose<TArgs extends any[], R1, R2, R3, R4, R5, R6, R7>(
   f7: (a: R6) => R7,
   f6: (a: R5) => R6,
@@ -62,6 +95,39 @@ export function compose<T>(...fns: Array<(arg: T) => T>) {
   return (x: T) => fns.reduceRight((acc, fn) => fn(acc), x);
 }
 
+export function pipe<TArgs extends any[], R1, R2, R3, R4, R5, R6, R7, R8, R9, R10>(
+  f1: (...args: TArgs) => R1,
+  f2: (a: R1) => R2,
+  f3: (a: R2) => R3,
+  f4: (a: R3) => R4,
+  f5: (a: R4) => R5,
+  f6: (a: R5) => R6,
+  f7: (a: R6) => R7,
+  f8: (a: R7) => R8,
+  f9: (a: R8) => R9,
+  f10: (a: R9) => R10,
+): (...args: TArgs) => R10;
+export function pipe<TArgs extends any[], R1, R2, R3, R4, R5, R6, R7, R8, R9>(
+  f1: (...args: TArgs) => R1,
+  f2: (a: R1) => R2,
+  f3: (a: R2) => R3,
+  f4: (a: R3) => R4,
+  f5: (a: R4) => R5,
+  f6: (a: R5) => R6,
+  f7: (a: R6) => R7,
+  f8: (a: R7) => R8,
+  f9: (a: R8) => R9,
+): (...args: TArgs) => R9;
+export function pipe<TArgs extends any[], R1, R2, R3, R4, R5, R6, R7, R8>(
+  f1: (...args: TArgs) => R1,
+  f2: (a: R1) => R2,
+  f3: (a: R2) => R3,
+  f4: (a: R3) => R4,
+  f5: (a: R4) => R5,
+  f6: (a: R5) => R6,
+  f7: (a: R6) => R7,
+  f8: (a: R7) => R8,
+): (...args: TArgs) => R8;
 export function pipe<TArgs extends any[], R1, R2, R3, R4, R5, R6, R7>(
   f1: (...args: TArgs) => R1,
   f2: (a: R1) => R2,
